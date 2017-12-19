12dbc5e837f8294db2257e77aca01516eaf739c1
Kapulara <bart.van.vliet@hotmail.com>
2017-12-19 21:55:22 +0100

    feat: created compilers for UML

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,19 @@ var gulp = require('gulp'),
     plantuml = require('gulp-plantuml');
 
 gulp.task('default', function () {
-    return gulp.src('./src/*.plantuml')
+    return gulp.src('src/**/*.plantuml')
         .pipe(plantuml({
             jarPath: "./vendor/plantuml.jar"
         }))
         .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('watch', ['default'], function () {
+    var watcher = gulp.watch('src/**/*.plantuml', ['default']);
+
+    watcher.on('change', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+
+    return watcher;
 });
